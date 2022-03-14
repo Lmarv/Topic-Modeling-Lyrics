c9967466a8419eeeadb908fd82fdb02c89f8534e
Alwine Balfanz <ab63cagu@studserv.uni-leipzig.de>
2022-03-14 10:05:26 +0100

    split preprocessing, topic modeling and visualization and add update check for genre indices

--- preprocessing.R
+++ preprocessing.R
@@ -27,6 +27,9 @@ data_only_lyrics$language <- textcat(data_only_lyrics[,5], p=myProfiles)
 #create new data frame containing only entries with English lyrics
 data_english <- data_only_lyrics %>% filter(grepl('english', language))
 
+#add song ids
+data_english$song_id <- 1:nrow(data_english)
+
 #create lyrics corpus
 lyrics_corpus <- corpus(data_english$lyrics, docnames = data_english$song_id)
 
@@ -76,168 +79,25 @@ sample_data <- data_english[sample_idx,]
 
 DTM <- sample_corpus_tokens %>%
   dfm() %>%
-  dfm_trim(min_docfreq = 0.01, max_docfreq = 0.99, docfreq_type = "prop")
+  dfm_trim(min_docfreq = 0.01, max_docfreq = 0.90, docfreq_type = "prop")
 
 sel_idx <- rowSums(DTM) > 0
 DTM <- DTM[sel_idx, ]
 sample_data <- sample_data[sel_idx, ]
 
-###################topic modeling#################################
-require(topicmodels)
-
-#best model so far
-
-K <- 12
-
-topicModel <- LDA(DTM, K, method = "Gibbs", control = list(iter = 500, alpha = 0.2,
-                                                           seed = 1, verbose = 25))
-
-tmResult <- posterior(topicModel)
-
-beta <- tmResult$terms
-
-theta <- tmResult$topics
-
-rowSums(theta)[1:12]
-
-terms(topicModel, 10)
-
-top5termsPerTopic <- terms(topicModel, 5)
-topicNames <- apply(top5termsPerTopic, 2, paste, collapse = " ")
-
-
-####################visualization wordclouds#########################################
-require(wordcloud2)
-
-#interesting topics to visualize as wordclouds, can be more or different ones
-
-topicToViz <- 3
-
-topicToViz <- grep("shit", topicNames)[1]
-
-top40terms <- sort(tmResult$terms[topicToViz, ], decreasing = TRUE)[1:40]
-words <- names(top40terms)
-
-probabilities <- sort(tmResult$terms[topicToViz, ], decreasing = TRUE)[1:40]
-
-wordcloud2(data.frame(words, probabilities), shuffle = FALSE)
-
-topicToViz2 <- 12
-
-topicToViz2 <- grep("love", topicNames2)[1]
-
-top40terms2 <- sort(tmResult$terms[topicToViz2, ], decreasing = TRUE)[1:40]
-words2 <- names(top40terms2)
-
-probabilities2 <- sort(tmResult$terms[topicToViz2, ], decreasing = TRUE)[1:40]
-
-wordcloud2(data.frame(words2, probabilities2), shuffle = FALSE)
-
-topicToViz3 <- 1
-
-topicToViz3 <- grep("night", topicNames)[1]
-
-top40terms3 <- sort(tmResult$terms[topicToViz3, ], decreasing = TRUE)[1:40]
-words3 <- names(top40terms3)
-
-probabilities3 <- sort(tmResult$terms[topicToViz3, ], decreasing = TRUE)[1:40]
-
-wordcloud2(data.frame(words3, probabilities3), shuffle = FALSE)
-
-########################### Topic proportions  ###################################################
-
-#This part inspects the proportions of all topics in the sample corpus.
-#First option gives the proportions of the topic in the sample corpus, second options calculates the number one topic in all documents,
-# listing all topics and how many documents (songs), have that topic as the most likely topic
-
-
-
-topicNames <- apply(lda::top.topic.words(beta, 5, by.score = T),
-                    2, paste, collapse = " ")
-
-topicProportions <- colSums(theta)/nrow(DTM)
-names(topicProportions) <- topicNames
-sort(topicProportions, decreasing = TRUE)
-
-countsOfPrimaryTopics <- rep(0, K)
-names(countsOfPrimaryTopics) <- topicNames
-for (i in 1:nrow(DTM)) {
-  topicsPerDoc <- theta[i, ]
-  primaryTopic <- order(topicsPerDoc, decreasing = TRUE)[1]
-  countsOfPrimaryTopics[primaryTopic] <- countsOfPrimaryTopics[primaryTopic] +
-    1
-}
-sort(countsOfPrimaryTopics, decreasing = TRUE)
-
-################################ visualization #############################################
-
-#Here the proportions of the topics are visualized per genre, only pop genre so far
-
-#creating empty vector for pop song ids
-exampleIds <- c()
-
-#getting the ids from pop_idx and concatenate them with 'text' in front of ids, step needed to get the right theta values from pop songs
-for(i in pop_idx){
-  str <- paste('text', as.character(i), sep= '')
-  exampleIds <- c(exampleIds, str)
-}
-  
-#creating new data frame with only pop song proportions of topics
-topicProportionExamples <- theta[exampleIds,]
-topicProportionExamples<- as.data.frame(topicProportionExamples)
-
-#name vector to rename columns
-columns <- c('Topic 1', 'Topic 2', 'Topic 3', 'Topic 4', 'Topic 5', 'Topic 6', 'Topic 7', 'Topic 8', 
-             'Topic 9', 'Topic 10', 'Topic 11', 'Topic 12')
-
-#initializing new data frame for calculated means of theta values for all topics over the whole pop song sample
-topicProportionsPopGenre = data.frame(matrix(nrow = 1, ncol = length(columns)))
-colnames(topicProportionsPopGenre) = columns
-
-#renaming columns, due to type conflicts
-names(topicProportionExamples)[names(topicProportionExamples)=='1'] <- 'Topic 1'
-names(topicProportionExamples)[names(topicProportionExamples)=='2'] <- 'Topic 2'
-names(topicProportionExamples)[names(topicProportionExamples)=='3'] <- 'Topic 3'
-names(topicProportionExamples)[names(topicProportionExamples)=='4'] <- 'Topic 4'
-names(topicProportionExamples)[names(topicProportionExamples)=='5'] <- 'Topic 5'
-names(topicProportionExamples)[names(topicProportionExamples)=='6'] <- 'Topic 6'
-names(topicProportionExamples)[names(topicProportionExamples)=='7'] <- 'Topic 7'
-names(topicProportionExamples)[names(topicProportionExamples)=='8'] <- 'Topic 8'
-names(topicProportionExamples)[names(topicProportionExamples)=='9'] <- 'Topic 9'
-names(topicProportionExamples)[names(topicProportionExamples)=='10'] <- 'Topic 10'
-names(topicProportionExamples)[names(topicProportionExamples)=='11'] <- 'Topic 11'
-names(topicProportionExamples)[names(topicProportionExamples)=='12'] <- 'Topic 12'
-
-#calculating means of theta values of each topic and store them in the correlating column
-topicProportionsPopGenre$`Topic 1` <- mean(topicProportionExamples$`Topic 1`)
-topicProportionsPopGenre$`Topic 2` <- mean(topicProportionExamples$`Topic 2`)
-topicProportionsPopGenre$`Topic 3` <- mean(topicProportionExamples$`Topic 3`)
-topicProportionsPopGenre$`Topic 4` <- mean(topicProportionExamples$`Topic 4`)
-topicProportionsPopGenre$`Topic 5` <- mean(topicProportionExamples$`Topic 5`)
-topicProportionsPopGenre$`Topic 6` <- mean(topicProportionExamples$`Topic 6`)
-topicProportionsPopGenre$`Topic 7` <- mean(topicProportionExamples$`Topic 7`)
-topicProportionsPopGenre$`Topic 8` <- mean(topicProportionExamples$`Topic 8`)
-topicProportionsPopGenre$`Topic 9` <- mean(topicProportionExamples$`Topic 9`)
-topicProportionsPopGenre$`Topic 10` <- mean(topicProportionExamples$`Topic 10`)
-topicProportionsPopGenre$`Topic 11` <- mean(topicProportionExamples$`Topic 11`)
-topicProportionsPopGenre$`Topic 12` <- mean(topicProportionExamples$`Topic 12`)
-
-library("reshape2")
-library("ggplot2")
-
-#renaming the columns to the top5 words of the topic
-colnames(topicProportionsPopGenre) <- topicNames  #-> können wir noch anders machen
-
-#visualize data as horizontal bar plots
-vizDataFrame <- melt(cbind(
-  data.frame(topicProportionsPopGenre),
-  pop = factor(1:1)),
-  variable.name = "topic", id.vars = "pop")
-
-ggplot(data = vizDataFrame,
-       aes(topic, value, fill = pop), ylab = "proportion") +
-  geom_bar(stat="identity") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  coord_flip() +
-  facet_wrap(~ pop, ncol = 1)
+#update genre indicies in order to avoid problems at visualization
+pop_idx <- intersect(pop_idx, sample_data$song_id)
+rap_idx <- intersect(rap_idx, sample_data$song_id)
+reggae_idx <- intersect(reggae_idx, sample_data$song_id)
+rnb_idx <- intersect(rnb_idx, sample_data$song_id)
+blues_idx <- intersect(blues_idx, sample_data$song_id)
+country_idx <- intersect(country_idx, sample_data$song_id)
+folk_idx <- intersect(folk_idx, sample_data$song_id)
+gospel_spiritual_idx <- intersect(gospel_spiritual_idx, sample_data$song_id)
+rock_idx <- intersect(rock_idx, sample_data$song_id)
+heavyMetal_idx <- intersect(heavyMetal_idx, sample_data$song_id)
+hipHop_idx <- intersect(hipHop_idx, sample_data$song_id)
+soul_idx <- intersect(soul_idx, sample_data$song_id)
+jazz_idx <- intersect(jazz_idx, sample_data$song_id)
+kids_idx <- intersect(kids_idx, sample_data$song_id)
 
