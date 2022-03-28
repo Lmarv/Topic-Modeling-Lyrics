da6ebce0ca03495131336cd463e79b4add66f343
Alwine Balfanz <ab63cagu@studserv.uni-leipzig.de>
2022-03-28 12:00:01 +0200

    add comments

--- genre_backtrace.R
+++ genre_backtrace.R
@@ -1,5 +1,7 @@
 library(dplyr)
 
+#function to calculate cosine similarity between a single song and the means of genres to find 
+#the most similar genres
 cos_sim <- function(song_id) {
   song <- theta[as.character(song_id),]
   blues_sim <- cosine(song, as.vector(t(topicProportionsBluesGenre)))
@@ -17,7 +19,7 @@ cos_sim <- function(song_id) {
   rock_sim <- cosine(song, as.vector(t(topicProportionsRockGenre)))
   soul_sim <- cosine(song, as.vector(t(topicProportionsSoulGenre)))
   
-  genre_sims <- data.frame(genre = c('blues', 'country', 'folk', 'gospel', 'heavy metal', 'hip-hop', 'jazz', 
+  genre_sims <- data.frame(genre = c('blues', 'country', 'folk', 'gospel', 'heavy metal', 'hip hop', 'jazz', 
                                      'kids', 'pop', 'rap', 'rnb', 'reggae', 'rock', 'soul'),
                            similarity = c(blues_sim, country_sim, folk_sim, gospel_sim, heavymetal_sim, hiphop_sim, 
                            jazz_sim, kids_sim, pop_sim, rap_sim, rnb_sim, reggae_sim, rock_sim, soul_sim))
@@ -26,6 +28,7 @@ cos_sim <- function(song_id) {
   return(top_3_genres$genre)
 }
 
+#create data frame including the correct genre and the top 3 most similar genres
 sim_df <- data.frame()
 
 for (s in sample_data$song_id) {
@@ -39,6 +42,7 @@ colnames(sim_df) <- c('song_id','genre','top1','top2','top3')
 sim_df$identified <- ifelse(sim_df$genre == sim_df$top1 | sim_df$genre == sim_df$top2 | 
                               sim_df$genre == sim_df$top3, TRUE, FALSE)
 
+#calculate the ratio of correctly identified genres
 correctly_identified <- length(which(sim_df$identified)) / length(sample_data)
 
 print(paste(paste("In", correctly_identified), "% of the songs the correct genres occur in the top 3 most similar genres regarding to the calculated theta values."))
